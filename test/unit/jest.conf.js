793d6a7fbe762f86d5fd65c4ef00b05f84ab2efb
jayden <jxdjayden@gmail.com>
2018-02-24 10:17:37 +0800

    build(location): update package.json

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -2,20 +2,16 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['js', 'vue'],
   moduleNameMapper: {
-    '@/([^\\.]*).vue$': '<rootDir>/src/$1.vue',
-    '@/([^\\.]*)$': '<rootDir>/src/$1.js',
-    '^vue$': 'vue/dist/vue.common.js',
-    '^packages/(.*).vue$': '<rootDir>/packages/$1.vue',
-    '^packages/(.*)$': '<rootDir>/packages/$1.js'
+    '@/([^\\.]*).vue$': '<rootDir>/packages/$1.vue',
+    '@/([^\\.]*)$': '<rootDir>/packages/$1.js',
+    '^vue$': 'vue/dist/vue.common.js'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  setupFiles: ['<rootDir>/test/unit/setup'],
-  coverageDirectory: '<rootDir>/test/unit/coverage',
-  collectCoverageFrom: ['!**/test/**', '!**/node_modules/**']
+  setupFiles: ['<rootDir>/test/unit/setup']
 }
