35a520e0e8cf61199aadc7428498bc484f6f9ca9
Aaron <aaron@AarondeMacBook-Pro.local>
2018-02-28 13:45:08 +0800

    feat(badge.vue): badge组件

--- examples/components/badge.vue
+++ examples/components/badge.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <section>
+    <div>
+      <ms-badge :value="'小标点'">我的学员</ms-badge>
+    </div>
+    <div>
+      <ms-badge :value="'hellow'">我的学员</ms-badge>
+    </div>
+    <div>
+      <ms-badge :value="100" :max="99">我的学员</ms-badge>
+    </div>
+    <div>
+      <ms-badge :value="1" :max="99">我的学员</ms-badge>
+    </div>
+    <div>
+      <ms-badge :isDot="true">我的学员</ms-badge>
+    </div>
+  </section>
 </template>
 <script>
 export default {
--- packages/badge/src/main.vue
+++ packages/badge/src/main.vue
@@ -1,9 +1,40 @@
 <template>
-<div></div>
+  <div class="ms-badge">
+    <slot></slot>
+    <sup
+      v-show="!hidden"
+      v-text="content"
+      class="ms-badge__content"
+      :class="{ 'is-dot': isDot }">
+    </sup>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MsBadge'
+  name: 'MsBadge',
+  props: {
+    value: {},
+    max: Number,
+    isDot: {
+      type: Boolean,
+      default: false
+    },
+    hidden: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    content() {
+      if (this.isDot) return;
+      const value = this.value;
+      const max = this.max;
+      if (typeof value === 'number' && typeof max === 'number') {
+        return max < value ? `${max}+` : value;
+      }
+      return value;
+    }
+  }
 };
 </script>
