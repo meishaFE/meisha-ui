cdc4ec11c70914ba7839462fdf4ef5f6ace84bbf
jayden <jxdjayden@gmail.com>
2018-03-30 11:22:25 +0800

    test(location): add checkboxGroup & checkbox test

--- test/unit/specs/checkbox-group.spec.js
+++ test/unit/specs/checkbox-group.spec.js
@@ -1,5 +1,27 @@
+import { createFactory } from '../utils'
+import MsCheckboxGroup from '@/checkbox-group/src/main.vue'
+
+const createCheckboxGroup = createFactory(MsCheckboxGroup)
+
 describe('checkbox-group', () => {
+  let wrapper
+  afterEach(() => {
+    wrapper && wrapper.destroy && wrapper.destroy()
+  })
   it('create', () => {
-    expect(true).toBeTruthy()
+    wrapper = createCheckboxGroup()
+    expect(wrapper.contains('.ms-checkbox-group')).toBeTruthy()
+  })
+
+  it('slot', () => {
+    wrapper = createCheckboxGroup(
+      {},
+      {
+        slots: {
+          default: '<div class="test-default-slot">test</div>'
+        }
+      }
+    )
+    expect(wrapper.contains('.test-default-slot')).toBeTruthy()
   })
 })
